d38f563adfbbcbedf3cbc9141625efaf72ff0d05
marcoslobo <wofeworks@gmail.com>
2024-01-15 10:34:48 -0300

    print mode alternate - exercise 2

--- exercise-2.py
+++ exercise-2.py
@@ -1,2 +1,5 @@
+# nome = input ('Qual é o seu nome?')
+# print ('Prazer em conhecer você,' + nome + '!')
+
 nome = input ('Qual é o seu nome?')
-print ('Prazer em conhecer você,' + nome + '!')
+print ('É um prazer te conhecer, {}!'.format(nome))
