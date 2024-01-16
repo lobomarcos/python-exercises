1fea08bf59f5c0698c36e4cb2c09d099956ace8c
marcoslobo <wofeworks@gmail.com>
2024-01-16 15:39:11 -0300

    fazendo somas

---
+++ exercise-3.py
@@ -0,0 +1,10 @@
+# TIPOS PRIMITIVOS
+# tipo 'int': são números inteiros.
+# tipo float: são números quebrados. ex.: 4.6, 0.7 etc.
+# tipo bool: apenas True or False.
+# tipo str: palavras e valores entre 'aspas', também existem vazias
+
+n1 = int(input('Digite um número:'))
+n2 = int(input('Digite mais um número:'))
+s = n1 + n2
+print('A soma vale', s)
