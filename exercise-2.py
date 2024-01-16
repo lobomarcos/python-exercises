fd0f3147cc95ad7ba4a06f7aaa05c6d88a6af8b1
marcoslobo <wofeworks@gmail.com>
2024-01-16 15:05:24 -0300

    more exercises

--- exercise-2.py
+++ exercise-2.py
@@ -3,3 +3,7 @@
 
 nome = input ('Qual é o seu nome?')
 print ('É um prazer te conhecer, {}!'.format(nome))
+idade = input ('Quantos anos você tem?')
+print ('Nossa! nem parece que você tem {}!'.format(idade))
+peso = input ('Quanto você pesa?')
+print ('Você parece magro para {}kg.'.format(peso))
