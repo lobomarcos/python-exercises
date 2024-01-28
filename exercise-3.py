ddcf78b328119c7b4e49b84d93f063f38fe3c322
marcoslobo <wofeworks@gmail.com>
2024-01-28 01:39:09 -0300

    lendo somas

--- exercise-3.py
+++ exercise-3.py
@@ -4,7 +4,14 @@
 # tipo bool: apenas True or False.
 # tipo str: palavras e valores entre 'aspas', também existem vazias
 
+# n1 = int(input('Digite um número:'))
+# n2 = int(input('Digite mais um número:'))
+# s = n1 + n2
+# print('A soma vale',s)
+# print('A soma entre ', n1, 'e', n2, 'vale', s)
+# print('A soma entre {} e {} vale {}' .format(n1, n2,s))
+
 n1 = int(input('Digite um número:'))
 n2 = int(input('Digite mais um número:'))
 s = n1 + n2
-print('A soma vale', s)
+print('A soma entre {} e {} é igual a {}.' .format(n1, n2, s)) 
