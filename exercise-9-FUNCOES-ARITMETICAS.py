ec970fa02889fd8ae43191223472520ec5378e56
marcoslobo <wofeworks@gmail.com>
2024-02-19 15:54:19 -0300

    learning functions

---
+++ exercise-9-FUNCOES-ARITMETICAS.py
@@ -0,0 +1,7 @@
+import math
+a = float(input('Me dê um número e eu te darei a raíz quadrada dele. Escreva-o:'))
+
+number = a
+raiz_quadrada = math.sqrt(number)
+
+print('A raíz quadrada de {} é {}.' .format(a, raiz_quadrada))
