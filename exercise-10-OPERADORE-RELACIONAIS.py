0f373024e3c002792803887023fba34535327674
marcoslobo <wofeworks@gmail.com>
2024-02-27 17:54:22 -0300

    learning more

---
+++ exercise-10-OPERADORE-RELACIONAIS.py
@@ -0,0 +1,46 @@
+'''
+igual
+a = int(6)
+b = int(2)
+r = a == b
+print('{} e {} são iguais? Resposta: {}' .format(a, b, r))
+'''
+
+'''
+diferentes
+a = int(7)
+b = int(3)
+r = a != b
+print('{} e {} são diferentes? Resposta: {}' .format(a, b, r))
+'''
+
+'''
+maior que
+a = int(5)
+b = int(8)
+r = a > b
+print('{} é maior que {}? Resposta: {}' .format(a, b, r))
+'''
+
+'''
+menor que
+a = int(8)
+b = int(48)
+r = a < b
+print('{} é menor que {}? Resposta: {}' .format(a, b, r))'''
+
+'''
+maior ou igual
+a = int(64)
+b = int(53)
+r = a >= b
+print ('{} é maior ou igual a {}? Resposta: {}' .format(a, b ,r))
+'''
+
+'''
+menor ou igual a
+a = int(34)
+b = int(123)
+r = a <= b
+print('{} é menor ou igual a {}? Resposta: {}' .format(a, b, r))
+'''
